35ffdcd5438b015bbca078cb9f31a251525bd112
Mordinii <allan.mordini@hotmail.com>
2022-09-02 11:01:53 -0300

    Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './Navegação/body/body.component';
+import { FooterComponent } from './Navegação/footer/footer.component';
+import { HeaderComponent } from './Navegação/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
